a239a3b395e0f278a8357b0a31ce1e4bf9e77d8f
Eli <61595010+elish266@users.noreply.github.com>
2023-07-02 15:16:24 +0300

    Add files via upload

--- step.sh
+++ step.sh
@@ -205,7 +205,7 @@ case $sign_method in
 						# keystore_file=$(download_file $BITRISE_CERTIFICATE_URL)
 						# keystore_pass=$BITRISE_CERTIFICATE_PASSPHRASE
 						
-						./appdome_api.sh --api_key $APPDOME_API_KEY \
+						echo --api_key $APPDOME_API_KEY \
 							--app $app_file \
 							--fusion_set_id $fusion_set_id \
 							$tm \
@@ -217,6 +217,21 @@ case $sign_method in
 							$bl \
 							--output $secured_app_output \
 							--certificate_output $certificate_output 
+
+
+
+						# ./appdome_api.sh --api_key $APPDOME_API_KEY \
+						# 	--app $app_file \
+						# 	--fusion_set_id $fusion_set_id \
+						# 	$tm \
+						# 	--sign_on_appdome \
+						# 	--keystore $keystore_file \
+						# 	--keystore_pass $keystore_pass \
+						# 	--provisioning_profiles $pf_list \
+						# 	$en \
+						# 	$bl \
+						# 	--output $secured_app_output \
+						# 	--certificate_output $certificate_output 
 							
 						;;
 esac
