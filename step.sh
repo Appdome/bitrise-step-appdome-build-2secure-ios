f1d04077f7416c0fb709590c2f5972c66e379b1d
Eli <61595010+elish266@users.noreply.github.com>
2023-07-02 18:17:35 +0300

    Add files via upload

--- step.sh
+++ step.sh
@@ -207,7 +207,6 @@ case $sign_method in
 							
 						;;
 "On-Appdome")			echo "On Appdome Signing"
-						echo BITRISE_CERTIFICATE_PASSPHRASE: $BITRISE_CERTIFICATE_PASSPHRASE
 						cf=$(convert_env_var_to_url_list $BITRISE_CERTIFICATE_URL)
 						cf_list=$(download_files_from_url_list $cf)
 						BK=$IFS
@@ -223,6 +222,19 @@ case $sign_method in
 							keystore_file=$cert_file
 							keystore_pass=${passwords[file_index]}
 						fi
+
+						echo --api_key $APPDOME_API_KEY \
+							--app $app_file \
+							--fusion_set_id $fusion_set_id \
+							$tm \
+							--sign_on_appdome \
+							--keystore $keystore_file \
+							--keystore_pass $keystore_pass \
+							--provisioning_profiles $pf_list \
+							$en \
+							$bl \
+							--output $secured_app_output \
+							--certificate_output $certificate_output 
 
 						./appdome_api.sh --api_key $APPDOME_API_KEY \
 							--app $app_file \
