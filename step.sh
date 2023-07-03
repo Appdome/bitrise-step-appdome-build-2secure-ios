64c4df4f9901c020c0e9e637060a319e10188e67
Eli <61595010+elish266@users.noreply.github.com>
2023-07-03 11:32:14 +0300

    Add files via upload

--- step.sh
+++ step.sh
@@ -168,12 +168,10 @@ pf=$(convert_env_var_to_url_list $BITRISE_PROVISION_URL)
 pf_list=$(download_files_from_url_list $pf)
 
 if [[ -n $provisioning_profiles ]]; then
-	# BK=$IFS
-	# IFS=","
-	# provisioning_profiles=$(echo $provisioning_profiles | sed -e 's/^[[:space:]]*//')
-	# IFS=""
-	# provisioning_profiles=${provisioning_profiles//" "/"_"}
-	# IFS=$BK
+	BK=$IFS
+	IFS=""
+	provisioning_profiles=${provisioning_profiles//" "/"_"}
+	IFS=$BK
 	create_custom_provisioning_list $provisioning_profiles	# returns provision_list
 	pf_list=$provision_list
 fi
