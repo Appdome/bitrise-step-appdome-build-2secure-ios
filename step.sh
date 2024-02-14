1fb2ff1d4fd20a6c3cbdbe3a365bc11592e0bd53
elish266 <elish266@gmail.com>
2024-02-14 13:51:59 +0200

    debug

--- step.sh
+++ step.sh
@@ -192,12 +192,11 @@ then
 	app_file=../$(download_file $app_location)
 else
 	app_file=$app_location
-	ls -al $app_location
+	echo "app_location: $app_location"
 	pwd
 	if [[ $app_location == *" "* ]];
 	then
 		app_file=${app_file//" "/"_"}
-		echo "app_location: $app_location"
 		echo "app_file: $app_file"
 		echo "cp $app_location $app_file"
 		cp $app_location $app_file
